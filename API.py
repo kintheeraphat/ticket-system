a419a20979f1146cea5f3a2fe6a50fb170d092a2
WachirawitBess <wachirawitbess@gmail.com>
2026-01-27 15:13:38 +0700

    ห

--- API.py
+++ API.py
@@ -1,11 +1,13 @@
 import requests
 
 url = "http://172.17.1.55:8111/erpAuth/"
+
 payload = {
     "username": "adminsp",
     "password": "tns2025"
 }
 
-response = requests.post(url, json=payload)
+response = requests.post(url, data=payload)
+
 print(response.status_code)
 print(response.json())
