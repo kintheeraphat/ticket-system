1e9b479468b2cb3a604c1245c6baae429246373c
WachirawitBess <wachirawitbess@gmail.com>
2026-02-23 15:42:22 +0700

    Setting...

--- Core/settings.py
+++ Core/settings.py
@@ -94,18 +94,7 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#         'OPTIONS': {
-#         'options': f"-c search_path={os.getenv('DB_SCHEMA')},public"}
-#     }
-# }
+
 
 
 # Password validation
