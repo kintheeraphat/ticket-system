6d92134c4376bc4f7a2aa701502ee5dab06e1282
WachirawitBess <wachirawitbess@gmail.com>
2026-02-23 15:41:38 +0700

    Setting

--- Core/settings.py
+++ Core/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'Core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),        # Tickets
-        'USER': os.getenv('DB_USER'),        # postgres
+        'NAME': os.getenv('DB_NAME'),        
+        'USER': os.getenv('DB_USER'),        
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),        # 172.17.1.199
-        'PORT': os.getenv('DB_PORT'),        # 5432
+        'HOST': os.getenv('DB_HOST'),        
+        'PORT': os.getenv('DB_PORT'),        
         'OPTIONS': {
             'options': f"-c search_path={os.getenv('DB_SCHEMA')}"
         },
